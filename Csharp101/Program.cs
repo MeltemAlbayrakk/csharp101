4ca249ddf3d64152adeed81ea7d581a8e52d3b8a
MeltemAlbayrakk <77754573+MeltemAlbayrakk@users.noreply.github.com>
2023-08-01 14:53:41 +0300

    switch-case

--- Csharp101/Program.cs
+++ Csharp101/Program.cs
@@ -1,2 +1,57 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'solve' function below.
+     *
+     * The function accepts following parameters:
+     *  1. DOUBLE meal_cost
+     *  2. INTEGER tip_percent
+     *  3. INTEGER tax_percent
+     */
+
+    public static void solve(double meal_cost, int tip_percent, int tax_percent)
+    {
+        double total_cost =0;
+        double tip ;
+        double tax ;
+        tip = tip_percent / 100 * meal_cost;
+        Console.Write("//"+tip);
+        tax = tax_percent / 100 * meal_cost;
+        Console.Write("//" + tax);
+        total_cost = tip + tax + meal_cost;
+        total_cost = Math.Round(total_cost, MidpointRounding.AwayFromZero);
+        Console.Write(total_cost);
+
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        double meal_cost = Convert.ToDouble(Console.ReadLine().Trim());
+
+        int tip_percent = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int tax_percent = Convert.ToInt32(Console.ReadLine().Trim());
+
+        Result.solve(meal_cost, tip_percent, tax_percent);
+    }
+}
